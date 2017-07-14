8df99ad3cc9282104ed2b4210f608b7c65bb151f
Alex Suraci <suraci.alex@gmail.com>
2017-07-14 11:13:04 -0400

    add InlineHTML

--- html.go
+++ html.go
@@ -7,7 +7,7 @@ import (
 	"io"
 )
 
-// HTML highlights a piece of code in HTML.
+// HTML highlights a piece of code in HTML <div class="highlight"><pre><code>.
 func HTML(lang string, code []byte) ([]byte, error) {
 	h, err := makeAndHighlight(lang, code)
 	if err != nil {
@@ -25,5 +25,26 @@ func HTML(lang string, code []byte) ([]byte, error) {
 		}
 	})
 	buf.Write([]byte(`</code></pre></div>`))
+	return buf.Bytes(), nil
+}
+
+// InlineHTML highlights a piece of code in HTML <code>.
+func InlineHTML(lang string, code []byte) ([]byte, error) {
+	h, err := makeAndHighlight(lang, code)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	buf.Write([]byte(`<code>`))
+	h.render(&buf, func(w io.Writer, class string, body []byte) {
+		escaped := html.EscapeString(string(body))
+		if len(class) > 0 {
+			fmt.Fprintf(w, "<span class=\"%s\">%s</span>", class, escaped)
+		} else {
+			w.Write([]byte(escaped))
+		}
+	})
+	buf.Write([]byte(`</code>`))
 	return buf.Bytes(), nil
 }
